46214b17c81132b72a996b2f6c00fe141cc7d514
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-10-30 14:34:14 +0300

    adding handle bars to communicate with layout works

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,23 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World here we come");
+        get("/hello", (req, res) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hello.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
