7550bc08d8f705aa6f635ba033e61fccc3d5e768
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-02 17:41:47 +0300

    tests are sufficient and passing

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,114 +1,86 @@
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class SquadTest {
-//
-//    @Test
-//    @DisplayName("Name getter")
-//    void getName() {
-//        Squad avengers = new Squad("Avengers",7,"starlabs");
-//        assertEquals("Avengers", avengers.getName());
-//    }
-//
-//    @Test
-//    @DisplayName("Name setter")
-//    void setName() {
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Size getter")
-//    void getSize() {
-//    }
-//
-//    @Test
-//    @DisplayName("Size setter")
-//    void setSize() {
-//    }
-//
-//    @Test
-//    @DisplayName("Cause getter")
-//    void getCause() {
-//    }
-//
-//    @Test
-//    @DisplayName("Cause setter")
-//    void setCause() {
-//    }
-//
-//    @Test
-//    @DisplayName("Id getter")
-//    void getId() {
-//    }
-//
-//    @Test
-//    @DisplayName("Id setter")
-//    void setId() {
-//    }
-//
-//    @Test
-//    @DisplayName("get appearance")
-//    void getAppearance() {
-//    }
-//
-//    @Test
-//    @DisplayName("set appearance ")
-//    void setAppearance() {
-//    }
-//
-//    @Test
-//    @DisplayName("Get a specific hero")
-//    void getHeroes() {
-//
-//    }
-//
-//    @Test
-//    void setHeroes() {
-//    }
-//
-//    @Test
-//    @DisplayName("add hero to Squad")
-//    void addHero() {
-//        Squad testSquad = new Squad("Justice League", 8,"World Justice");
-//        Hero batman = new Hero("Batman","Rich","joker and martha",testSquad.getId());
-//        Hero wonderwoman = new Hero("Wonderwoman","amazonian","aeries", testSquad.getId()) ;
-//        Hero superman = new Hero("Superman","flight,strength,laser", "kryptonite",
-//                testSquad.getId());
-//        List<Hero> expectedOutput = new ArrayList<Hero>();
-//        expectedOutput.add(batman);
-//        expectedOutput.add(wonderwoman);
-//        expectedOutput.add(superman);
-//        assertTrue(testSquad.getHeroes().contains(batman));
-//        assertFalse(testSquad.getHeroes().contains(superman));
-//    }
-//
-//    @Test
-//    void removeHero() {
-//    }
-//
-//    @Test
-//    void removeHeroes() {
-//    }
-//
-//    @Test
-//    void clearSquads() {
-//    }
-//
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @AfterEach
+    public void tearDown() {
+        Hero.clearAll();
+    }
+
+    @Test
+    @DisplayName("Name getter")
+    void getName() {
+        Squad avengers = new Squad("Avengers",7,"starlabs");
+        assertEquals("Avengers", avengers.getName());
+    }
+
+    @Test
+    @DisplayName("Name setter")
+    void setName() {
+        Squad avengers = new Squad("",7,"starlabs");
+        avengers.setName("Avengers");
+        assertEquals("Avengers", avengers.getName());
+
+    }
+
+    @Test
+    @DisplayName("Size getter")
+    void getSize() {
+        Squad avengers = new Squad("Avengers",7,"starlabs");
+        assertEquals(7, avengers.getSize());
+    }
+
+    @Test
+    @DisplayName("Size setter")
+    void setSize() {
+        Squad avengers = new Squad("Avengers",0,"starlabs");
+        avengers.setSize(7);
+        assertEquals(7, avengers.getSize());
+    }
+
+    @Test
+    @DisplayName("Cause getter")
+    void getCause() {
+        Squad avengers = new Squad("Avengers",7,"starlabs");
+        assertEquals("starlabs", avengers.getCause());
+    }
+
+    @Test
+    @DisplayName("Cause setter")
+    void setCause() {
+        Squad avengers = new Squad("Avengers",7,"");
+        avengers.setCause("starlabs");
+        assertEquals("starlabs", avengers.getCause());
+    }
+
+    @Test
+    @DisplayName("add hero to Squad")
+    void addHero() {
+        Squad testSquad = new Squad("Justice League", 8,"World Justice");
+        Hero batman = new Hero("Batman","Rich","joker and martha",testSquad.getId());
+        Hero wonderwoman = new Hero("Wonderwoman","amazonian","aeries", testSquad.getId()) ;
+        Hero superman = new Hero("Superman","flight,strength,laser", "kryptonite",
+                testSquad.getId());
+        List<Hero> expectedOutput = new ArrayList<Hero>();
+        expectedOutput.add(batman);
+        expectedOutput.add(wonderwoman);
+        expectedOutput.add(superman);
+        assertTrue(testSquad.getHeroes().contains(batman));
+    }
+
 //    @Test
 //    @DisplayName("Gets a Hero")
 //    void find_returnsCorrectHeroFromAList_Hero() {
 //        Squad bigHero6 = new Squad("BigHero6",6,"Science");
 //        Squad smash = new Squad("SMASH", 6,"Harmony");
-//        assertEquals(smash,Squad.find(2));
+//        assertEquals(smash,Squad.find(1));
 //    }
-//
-//    @Test
-//    @DisplayName("Get all squads in a list")
-//    void getSquads() {
-//    }
-//}
+
+}
